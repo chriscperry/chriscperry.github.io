5d549916a6845ecc4435be462482c7c80c0cd062
Chris Perry <chris@chrisperry.org>
2017-02-11 19:36:25 -0500

    added cover slides

--- slideshow.js
+++ slideshow.js
@@ -1,7 +1,22 @@
-var images = [];
-var bg = "#FF00FF"
+var slides = [];
+var bg = 255
+
+var desX = 0,
+    imgX = 0;
+
+var index = 0;
+
 
 function preload() {
+println("loading images...")
+slides = [loadImage("assets/portableEnvironmentsCover.png"),createVideo("assets/Broccoli.mp4"),createVideo("assets/wis.mp4")]
+titles = ["Portable","Scale in","Procedural"]
+
+slides[1].loop();
+slides[1].hide();
+
+slides[2].loop();
+slides[2].hide();
 
 }
 
@@ -18,6 +33,52 @@ function setup() {
 }
 
 function draw() {
-	setupCanvas()
+	setupCanvas();
+    rectMode(CENTER);
+    drawSlides();
+    drawTitle(index);
+
+
+}
+
+
+
+function mousePressed() {
+    desX -= width;
+    index++;
+}
+
+function drawSlides() {
+    imgX += .05 * (desX - imgX);
+    push();
+    noStroke();
+    translate(imgX, 0);
+    rectMode(CENTER)
+    textAlign(CENTER);
+    for(var x = 0; x < slides.length; x++){
+    	drawSlide(x,width * x,0)
+    }
+    pop();
+}
+
+function drawSlide(n,x,y){
+    image(slides[n],x,y,width,height)
+}
+
+function drawTitle(n){
+    fill(255);
+    textSize(100);
+    textAlign(RIGHT);
+    t = ""
+    if(n < titles.length){
+        t = titles[n]
+    }
+    text(t,width * .45, height*.5);
+    textAlign(LEFT);
+    text(" Environments",width * .45, height*.5);
+
+
+
+
 
 }
